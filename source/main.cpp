9807f6ad00066e807bceb5d2bbe9866dfcd5bae8
ZeyzerDeIx <71116684+ZeyzerDeIx@users.noreply.github.com>
2025-02-11 12:02:58 +0100

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,13 @@
+#include <nds.h>
+
+int main() {
+
+  /* This program doesn't do very much, yet. */
+
+  /* Loop forever so that the Nintendo DS doesn't reboot upon program
+   * completion. */
+  for (;;)
+    ;
+
+  return 0;
+}
