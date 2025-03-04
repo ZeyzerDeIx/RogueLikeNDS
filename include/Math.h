d0fa3a58fed287088379e38728cbbdd0fea470f7
ZeyzerDeIx <71116684+ZeyzerDeIx@users.noreply.github.com>
2025-03-04 13:13:12 +0100

    feat (Vector2) : add operators

--- include/Math.h
+++ include/Math.h
@@ -5,6 +5,16 @@ struct Vector2
 {
 	T x;
 	T y;
+
+	Vector2<T> operator+(const Vector2<T>& other)
+	{ return {x+other.x, y+other.y}; }
+	Vector2<T> operator-(const Vector2<T>& other)
+	{ return {x-other.x, y-other.y}; }
+
+	void operator+=(const Vector2<T>& other)
+	{ x+=other.x; y+=other.y; }
+	void operator-=(const Vector2<T>& other)
+	{ x-=other.x; y-=other.y; }
 };
 
 using Vector2f = Vector2<float>;
