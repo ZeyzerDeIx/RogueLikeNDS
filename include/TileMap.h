3a75797f2ab9d0920ec83cb5b786fd7fb5fdf825
ZeyzerDeIx <71116684+ZeyzerDeIx@users.noreply.github.com>
2025-03-04 01:32:55 +0100

    feat (tilemap): add TileMap class

---
+++ include/TileMap.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "PCH.h"
+
+class TileMap
+{
+public:
+	// size in tiles
+	TileMap(Vector2i size);
+	~TileMap();
+
+	//flush the map into background VRAM
+	void flush();
+
+	std::vector<u8>& operator[](int key);
+private:
+	std::vector<std::vector<u8>> m_tileMap;
+	// real background accurate tilemap based on sub tiles made by grit
+	static u8 m_bgTileMap[SUB_TILE::COUNT_W][SUB_TILE::COUNT_H];
+
+	//Convert m_tileMap into a background adapted tilemap
+	void convertMap();
+};
--- source/main.cpp
+++ source/main.cpp
@@ -1,29 +1,20 @@
-#include <nds.h>
 #include <stdio.h>
+#include "PCH.h"
+#include "TileMap.h"
 #include "TileSet.h"
-
-// --- Données de démonstration ---
-// Remplace ces tableaux par les données générées par ton outil (grit, gfx2gba, Usenti, etc.)
-
-// 📌 Constantes pour la configuration du background
-#define BACKGROUND_ID  2
-#define BG_TYPE        BgType_Rotation
-#define BG_SIZE        BgSize_R_256x256
-#define CHARBLOCK      0
-#define MAPBLOCK       1
 
 
 int main(void) {
-	// Initialisation du DS en mode 2D (mode 0 pour les backgrounds tilemaps)
+	// Initialisation du DS en mode 2D (mode 2 pour les backgrounds tilemaps affine)
 	videoSetMode(MODE_2_2D);
 	// Affecte VRAM_A au background principal
 	vramSetBankA(VRAM_A_MAIN_BG);
 
 	consoleDemoInit();
 	
-	// Initialise le background 0 en mode tilemap 256x256 (32x32 tuiles)
+	// Initialise le background en mode tilemap 256x256 (32x32 tuiles)
 	// Les paramètres : (numéro, type, taille, map offset, tile offset)
-	int bg0 = bgInit(BACKGROUND_ID, BG_TYPE, BG_SIZE, CHARBLOCK, MAPBLOCK);
+	int bg0 = bgInit(BG::ID, BG::TYPE, BG::SIZE, 0, 1);
 	
 	// Chargement des données en VRAM :
 	// Charger le tileset en VRAM
@@ -32,60 +23,27 @@ int main(void) {
 	// Charger la palette
 	dmaCopy(TileSetPal, BG_PALETTE, TileSetPalLen);
 
-	swiWaitForVBlank();
+	TileMap tileMap({TILE::COUNT_W,TILE::COUNT_H});
+	tileMap[0][1] = 2;
+	tileMap[0][2] = 2;
+	tileMap[1][0] = 1;
+	tileMap[2][0] = 1;
+	tileMap[1][1] = 4;
+	tileMap[2][1] = 4;
+	tileMap[1][2] = 4;
+	tileMap[2][2] = 4;
+	tileMap.flush();
 
-	u8 *mapPtr = (u8*)bgGetMapPtr(bg0);
-	static u8 tileMap[32*24] = 
-{
-	0 ,1 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	16,17,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
-	0 ,0 ,0 ,1 ,2 ,3 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 
-};
-
-	//DC_InvalidateAll();
-//
-	//u8 i = 0;
-	//for(i=0 ; i < 32 ; i++)
-	//{
-	//	mapPtr[i] = i%8;
-	//	printf("%d : ", mapPtr[i]);
-	//}
-//
-	//DC_FlushAll();
-	//dmaCopy(tileMap, mapPtr, 16 * 24);
-	//dmaCopy(tileMap, mapPtr, sizeof(tileMap)>>1);
-
-	//mapPtr[0] = 0;
-	//mapPtr[1] = 1;
-	//mapPtr[32] = 16;
-	//mapPtr[33] = 17;
 	
 	int x = 0, y = 0;
 	int cx = 0, cy = 0;
 
 	int angle = 10;
 	int scale = 1 << 8;
+	#if __cplusplus >= 202002L
+   	printf("C++20 enabled\n");
+	#endif
+
 
 	while (1)
 	{
@@ -118,8 +76,9 @@ int main(void) {
 		else if (keys_held & KEY_START)
 			scale -= 1 << 3;
 
+
 		if (keys_held & KEY_X)
-			swiFastCopy(tileMap, mapPtr, sizeof(tileMap)>>2);
+			tileMap.flush();
 
 		bgSetCenter(bg0, cx, cy);
 		bgSetRotateScale(bg0, angle, scale, scale);
