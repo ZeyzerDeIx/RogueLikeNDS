80a7f5c0e6ea5acf504b4c7f07af1de167efc646
ZeyzerDeIx <71116684+ZeyzerDeIx@users.noreply.github.com>
2025-10-19 13:27:58 +0200

    refacto(GameManager): delete useless comments

--- source/GameManager.cpp
+++ source/GameManager.cpp
@@ -12,9 +12,6 @@ void GameManager::initAll()
 	vramSetBankB(VRAM_B_MAIN_BG_0x06020000);
 	vramSetBankC(VRAM_C_SUB_BG_0x06200000);
 	vramSetBankD(VRAM_D_MAIN_BG_0x06060000);
-	//vramSetBankE(VRAM_E_MAIN_SPRITE);
-	//vramSetBankF(VRAM_F_MAIN_SPRITE_0x06410000);
-	//vramSetBankG(VRAM_G_MAIN_SPRITE_0x06414000);
 	
 	bgInit(BG::ID, BG::TYPE, BG::SIZE, 0, 1);
 }
