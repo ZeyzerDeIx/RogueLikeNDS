6769f450fa00364247e59d13a1c24fcccdd20291
ZeyzerDeIx <71116684+ZeyzerDeIx@users.noreply.github.com>
2025-12-08 23:38:56 +0100

    feat (Debug): add nocashLog and nocashLogF to debug class

--- include/Debug.h
+++ include/Debug.h
@@ -1,4 +1,8 @@
 #pragma once
+
+#include <cstdio>
+#include <cstdarg>
+
 class Entity;
 class Debug
 {
@@ -14,6 +18,18 @@ public:
 	void beginProfile();
 	void endProfile();
 	void logProfile();
+	static void nocashLog(const char* logTxt) { nocashMessage(logTxt); }
+	static void nocashLogF(const char* format, ...)
+	{
+	    char buffer[256];
+	    
+	    va_list args;
+	    va_start(args, format);
+	    vsnprintf(buffer, sizeof(buffer), format, args);
+	    va_end(args);
+	    
+	    nocashMessage(buffer);
+	}
 private:
 	Debug();
 	~Debug(){};
