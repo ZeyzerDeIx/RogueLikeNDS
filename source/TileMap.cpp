b0e36d7cf854e348910628f49d2854a93590ff52
ZeyzerDeIx <71116684+ZeyzerDeIx@users.noreply.github.com>
2026-09-11 19:51:39 +0200

    IA ring buffer

--- source/TileMap.cpp
+++ source/TileMap.cpp
@@ -3,75 +3,14 @@
 
 u16 TileMap::m_BgTileMap[SUB_TILE::COUNT_W][SUB_TILE::COUNT_H];
 
-TileMap::TileMap(): 
-	m_TileMap(META_TILE::COUNT_W, std::vector<MetaTile>(META_TILE::COUNT_H))
+TileMap::TileMap()
 {
-	// Load tiles data in memory
 	swiFastCopy(TileSetTiles, bgGetGfxPtr(BG::ID), TileSetTilesLen>>2);
 	swiFastCopy(TileSetPal, BG_PALETTE, TileSetPalLen>>2);
-
-	CalculateConnections();
 }
 
 void TileMap::Flush()
 {
-	ConvertMap();
-	dmaCopyAsynch(m_BgTileMap, bgGetMapPtr(BG::ID), sizeof(m_BgTileMap));
-}
-
-std::vector<MetaTile>& TileMap::operator[](int key)
-{
-	return m_TileMap[key];
-}
-
-
-
-void TileMap::ConvertMap()
-{
-	int rows = m_TileMap.size() - WORD_BORDER_SIZE;
-	int cols = m_TileMap[0].size() - WORD_BORDER_SIZE;
-
-	for (int i = WORD_BORDER_SIZE ; i < rows ; ++i)
-		for (int j = WORD_BORDER_SIZE ; j < cols ; ++j)
-			m_TileMap[i][j].Flush(m_BgTileMap, {i, j});
-}
-
-
-void TileMap::CalculateConnections()
-{
-	using namespace META_TILE;
-	using namespace DIRECTION;
-
-	int rows = m_TileMap.size() - WORD_BORDER_SIZE;
-	int cols = m_TileMap[0].size() - WORD_BORDER_SIZE;
-
-	for (int y = WORD_BORDER_SIZE; y < rows; ++y)
-	{
-		for (int x = WORD_BORDER_SIZE; x < cols; ++x)
-		{
-			MetaTile& tile = m_TileMap[y][x];
-			const Type& type = tile.GetType();
-			int con = 0;
-
-			// This lambda is here to compress the code, it check if the tile type is the same as the one of the wanted other tile
-			auto t = [&](int X, int Y){ return m_TileMap[X][Y].GetType() == type; };
-
-			// Precompute boundaries to avoid redundant checks
-			bool top = y > 0, bot = y < rows-1, left = x > 0, right = x < cols-1;
-
-			// Cardinal directions
-			if (top   && t(y-1, x)) con |= TOP;
-			if (bot   && t(y+1, x)) con |= BOT;
-			if (left  && t(y, x-1)) con |= LEFT;
-			if (right && t(y, x+1)) con |= RIGHT;
-
-			// Diagonal directions
-			if (top && left  && t(y-1, x-1)) con |= TOP_LEFT;
-			if (top && right && t(y-1, x+1)) con |= TOP_RIGHT;
-			if (bot && left  && t(y+1, x-1)) con |= BOT_LEFT;
-			if (bot && right && t(y+1, x+1)) con |= BOT_RIGHT;
-
-			tile.SetConnections(con);
-		}
-	}
+	// Copie SYNCHRONE (dmaCopy et non Asynch) : garantit l'absence de clipping si appelé en VBlank !
+	dmaCopy(m_BgTileMap, bgGetMapPtr(BG::ID), sizeof(m_BgTileMap));
 }
