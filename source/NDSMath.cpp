ab572af7bc03fa5b575d0d84dfd3c23981687216
ZeyzerDeIx <71116684+ZeyzerDeIx@users.noreply.github.com>
2026-09-13 11:45:55 +0200

    Remove unused functions

--- source/NDSMath.cpp
+++ source/NDSMath.cpp
@@ -1,17 +1,5 @@
 #include "NDSMath.h"
 
-int NDSMath::RoundAbsCeil(int x)
-{
-	return x >= 0 ?
-		((x + (1 << FIXED_POINT_SHIFT) - 1) >> FIXED_POINT_SHIFT) :
-		-(((-x) + (1 << FIXED_POINT_SHIFT) - 1) >> FIXED_POINT_SHIFT);
-}
-
-Vector2i NDSMath::RoundAbsCeil(Vector2i vec)
-{
-	return {RoundAbsCeil(vec.x), RoundAbsCeil(vec.y)};
-}
-
 int NDSMath::ToFixedPointInt(int x)
 {
 	return x << FIXED_POINT_SHIFT;
